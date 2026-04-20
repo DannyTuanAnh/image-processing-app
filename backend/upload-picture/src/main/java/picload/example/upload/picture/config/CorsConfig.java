e11a2cbdf455013ea4cc152b55623d948f6379c3
Hoang Phuong <155792523+DiazMarco118@users.noreply.github.com>
2026-04-20 21:03:55 +0700

    Thay doi config cors khop voi fe

--- backend/upload-picture/src/main/java/picload/example/upload/picture/config/CorsConfig.java
+++ backend/upload-picture/src/main/java/picload/example/upload/picture/config/CorsConfig.java
@@ -1,10 +1,10 @@
 package picload.example.upload.picture.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -15,11 +15,12 @@ import java.util.List;
         {
             CorsConfiguration config = new CorsConfiguration();
 
-            config.setAllowedOrigins(List.of("http://localhost:5713"));
+            config.setAllowedOrigins(List.of("http://localhost:5173"));
             config.setAllowedHeaders(List.of("*"));
             config.setAllowedMethods(List.of("*"));
+            config.setAllowCredentials(true);
             UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
             source.registerCorsConfiguration("/**", config);
-            return new CorsFilter();
+            return new CorsFilter(source);
     }
     }
